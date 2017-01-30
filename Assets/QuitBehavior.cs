87915d7866e0e61a11c3a888f66c73fe4c43c9ac
Markus Weber <marweb@alice.de>
2017-01-30 23:34:19 +0100

    added quit and restart functionalities to pause menu

---
+++ Assets/QuitBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Quit()
+    {
+        // Save game data
+
+        // Close game
+        Application.Quit();
+    }
+}
