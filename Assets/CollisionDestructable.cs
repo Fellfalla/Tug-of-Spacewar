29265db334e55dd1c3cd7e14b527f732b77cba45
Markus Weber <marweb@alice.de>
2017-01-31 01:27:01 +0100

    added destruction and explosion to rocket

---
+++ Assets/CollisionDestructable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDestructable : MonoBehaviour
+{
+
+    public float MaxImpactVelocity;
+    public GameObject Explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        float maxSqForce = MaxImpactVelocity*MaxImpactVelocity;
+        if (maxSqForce < coll.relativeVelocity.sqrMagnitude)
+        {
+            var instance = Object.Instantiate(Explosion, gameObject.transform.position, gameObject.transform.rotation);
+            Object.Destroy(gameObject);
+        }
+    }
+}
